11d90a9291cefd5a4d1914553f7348ff83470f3b
Rohit Kaushik <rohitkaushik12801@gmail.com>
2026-06-20 12:54:31 +0530

    Second Stage: Navbar Fix

--- Gallery/src/components/Navbar.jsx
+++ Gallery/src/components/Navbar.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='bg-mist-800  h-15 w-full flex justify-between items-center px-8 py-1'>
+    <div className='bg-mist-800  h-15 w-full flex justify-center items-center px-8 py-1 sm:justify-between'>
  
         <h1 className='text-amber-400 font-semibold text-3xl'>Photo Gallery</h1>
 
-        <ul className='flex gap-7 text-amber-300 text-balance font-sans text-xl'>
+        <ul className='flex gap-7 text-amber-300 text-balance font-sans text-xl hidden  sm:flex'>
             <li className='cursor-pointer'>Home</li>
             <li className='cursor-pointer'>Gallery</li>
             <li className='cursor-pointer'>Contact</li>
