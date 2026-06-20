c2332def9b39089f8631cb3fdc765c091ebbc2ab
Rohit Kaushik <rohitkaushik12801@gmail.com>
2026-06-20 12:58:20 +0530

    Second Stage : Navbar Fix V.01

--- Gallery/src/components/Navbar.jsx
+++ Gallery/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
  
         <h1 className='text-amber-400 font-semibold text-3xl'>Photo Gallery</h1>
 
-        <ul className='flex gap-7 text-amber-300 text-balance font-sans text-xl hidden  sm:flex'>
+        <ul className='gap-7 text-amber-300 text-balance font-sans text-xl hidden sm:flex'>
             <li className='cursor-pointer'>Home</li>
             <li className='cursor-pointer'>Gallery</li>
             <li className='cursor-pointer'>Contact</li>
